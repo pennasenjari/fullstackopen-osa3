1452c93ca59bdaf8ede4d24a5701581f2919850f
Jari Pennanen <pennasenjari@gmail.com>
2023-07-03 18:57:44 +0300

    Part 3, tasks 3.13 - 3.14

--- index.js
+++ index.js
@@ -9,91 +9,64 @@ morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
-
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
+const Person = require('./models/person')
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
+  Person.findById(req.params.id).then(person => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
 
-  res.status(204).end()
+  Person.deleteOne({ _id: req.params.id }).then(res2 => {
+    res.send(res2)
+  }).catch(err => {
+    console.log(err)
+    return res.status(400).json({ 
+      error: 'An error occurred' 
+    })
+  })
 })
 
-const generateId = () => {
-  min = 1
-  max = 1000000000000
-  return Math.floor(Math.random() * (max - min) + min)
-}
-
 app.post('/api/persons', (req, res) => {
-
   const body = req.body
+
   if (!body.name || !body.number) {
     return res.status(400).json({ 
       error: 'Both name and number are required' 
     })
   }
+
   const newName = req.body.name
   const newNumber = req.body.number
-  const existingPerson = persons.find(p => p.name.toLowerCase() === newName.toLowerCase())
-  if (existingPerson) {
-    return res.status(400).json({ 
-      error: `${newName} is already in phonebook`
-    })
-  }
-  const person = {
+  const person = new Person({
     name: newName,
-    number: body.number,
-    id: generateId(),
-  }
-  persons = persons.concat(person)
-  res.json(person)
+    number: newNumber
+  })
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 app.get('/info', (req, res) => {
   const now  =  new Date()
-  const ret = `<p>Phonebook has info for ${persons.length} people.</p><p>${now.toString()}</p>`
-  res.send(ret)
+
+  Person.count().then(count => {
+    const ret = `<p>Phonebook has info for ${count} people.</p><p>${now.toString()}</p>`
+    res.send(ret)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
