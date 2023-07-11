e94640ec8576b744a6ddbbd0ec9e83c3aad2210e
Jari Pennanen <pennasenjari@gmail.com>
2023-07-11 12:11:24 +0300

    Part 3, tasks 3.19 - 3.21

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -66,7 +68,7 @@ app.delete('/api/persons/:id', (req, res, next) => {
     })
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -85,6 +87,7 @@ app.post('/api/persons', (req, res) => {
   person.save().then(savedPerson => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
--- models/person.js
+++ models/person.js
@@ -14,8 +14,23 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        // ChatGPT helped to construct the regexp pattern!
+        return /^(?=.{8,}$)\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }  
 })
 
 personSchema.set('toJSON', {
